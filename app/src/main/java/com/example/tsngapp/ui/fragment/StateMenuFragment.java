70fbf8ea6cedc1c24d1c28c2ece3378f4868b5a0
José Pereira <jmfp-b@live.com.pt>
2019-11-25 18:49:33 +0000

    Small changes

--- app/src/main/java/com/example/tsngapp/ui/fragment/StateMenuFragment.java
+++ app/src/main/java/com/example/tsngapp/ui/fragment/StateMenuFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,7 +25,7 @@ public class StateMenuFragment extends BaseFragment {
     protected void onCreateViewActions(@NonNull LayoutInflater inflater,
                                        @Nullable ViewGroup container,
                                        @Nullable Bundle savedInstanceState) {
-        setupStaticResources();
+        setupMenuButtons();
     }
 
     @Override
@@ -45,7 +44,7 @@ public class StateMenuFragment extends BaseFragment {
         }
     }
 
-    private void setupStaticResources() {
+    private void setupMenuButtons() {
         final View currentMenuItemView = rootView.findViewById(R.id.menu_item_current);
         ((ImageView) currentMenuItemView.findViewById(R.id.iv_item_state_menu_icon))
                 .setImageResource(R.drawable.ic_power_black_24dp);
