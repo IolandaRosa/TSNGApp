3879a2322e8963750826dc3e28fc0069b7d77312
José Pereira <jmfp-b@live.com.pt>
2019-12-16 20:18:16 +0000

    Added a division spinner to the division and light state fragments

--- app/src/main/java/com/example/tsngapp/ui/fragment/DivisionStateFragment.java
+++ app/src/main/java/com/example/tsngapp/ui/fragment/DivisionStateFragment.java
@@ -1,9 +1,13 @@
 package com.example.tsngapp.ui.fragment;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -43,8 +47,12 @@ public class DivisionStateFragment extends BaseNestedFragment {
     }
 
     private void loadListData() {
+        this.loadListData("all");
+    }
+
+    private void loadListData(String division) {
         refreshLayout.setRefreshing(true);
-        new SMARTAAL.DivisionValues(AuthManager.getInstance().getElder().getId(), "all",
+        new SMARTAAL.DivisionValues(AuthManager.getInstance().getElder().getId(), division,
             AuthManager.getInstance().getUser().getAcessToken(), r -> {
                 listAdapter.setList(r);
                 refreshLayout.setRefreshing(false);
@@ -71,6 +79,18 @@ public class DivisionStateFragment extends BaseNestedFragment {
         refreshLayout = rootView.findViewById(R.id.srl_fragment_state);
         refreshLayout.setOnRefreshListener(this::loadListData);
         rvStateList = rootView.findViewById(R.id.rv_division_state);
+        ((Spinner) rootView.findViewById(R.id.sp_divisions))
+            .setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    String[] divisions = getResources().getStringArray(R.array.array_divisions);
+                    final String division = divisions[position].toLowerCase();
+                    loadListData(division);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {}
+            });
     }
 }
 
